edfc2e630c9e19a2781a374e7b4960cc59eac998
MotaGuilherme <metatronmota@gmail.com>
2023-05-30 17:19:05 -0300

    all

---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,70 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rook extends ChessPiece {
+
+    public Rook(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "R";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0,0);
+
+        // above
+        p.setValues(p.getRow() - 1, p.getColumn());
+        while (getBoard().positonExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() - 1);
+        }
+        if (getBoard().positonExists(p) && isThereOponnentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // left
+
+        p.setValues(p.getRow(), p.getColumn() - 1);
+        while (getBoard().positonExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getColumn() - 1);
+        }
+        if (getBoard().positonExists(p) && isThereOponnentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // right
+
+        p.setValues(p.getRow(), p.getColumn() + 1);
+        while (getBoard().positonExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getColumn() + 1);
+        }
+        if (getBoard().positonExists(p) && isThereOponnentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // below
+
+        p.setValues(p.getRow() + 1, p.getColumn());
+        while (getBoard().positonExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() + 1);
+        }
+        if (getBoard().positonExists(p) && isThereOponnentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        return mat;
+    }
+}
